fc84b1c1cb5561780635749845af6834743eb60b
ss-karthik <maniac.sskarthik@gmail.com>
2025-09-20 19:20:06 +0530

    Add Urinalysis demo

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import authRouter from "./routes/auth.js";
 import ctRouter from "./routes/ct/ct.js"
+import urinalysisRouter from "./routes/urinalysis/urinalysis.js";
 import dotenv from "dotenv";
 import cors from "cors"
 dotenv.config()
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use('/auth/v1', authRouter);
 app.use('/ct/v1', ctRouter);
+app.use('/urinalysis/v1', urinalysisRouter);
 
 
 app.listen(port, ()=>{
---
+++ server/src/routes/urinalysis/urinalysis.js
@@ -0,0 +1,25 @@
+import express, {} from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import path from "path";
+import { PrismaClient } from "@prisma/client";
+const urinalysisRouter = express.Router();
+urinalysisRouter.post('/upload', async (req, res) => {
+    const data = req.body;
+    const tosend = { "data": data };
+    console.log(data);
+    const pythonresp = await fetch("https://maniakss.pythonanywhere.com/api/analyse", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(tosend)
+    });
+    const result = await pythonresp.json();
+    res.status(200).json({
+        message: 'Analysis Generated successfully!',
+        analysis: result.response,
+    });
+});
+export default urinalysisRouter;
+//# sourceMappingURL=urinalysis.js.map
